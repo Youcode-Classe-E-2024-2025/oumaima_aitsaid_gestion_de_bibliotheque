0f80c0134eadbbbd0dd4d15f0db7350dd3773754
OUMAIMA-AIT-SAID <oumaimaaitsaid.dev@gmail.com>
2025-02-21 15:48:32 +0100

    add routting

--- gestion_bibliotheque/app/Http/Controllers/Controller.php
+++ gestion_bibliotheque/app/Http/Controllers/Controller.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
-abstract class Controller
+use Illuminate\Routing\Controller as BaseController;
+
+class Controller extends BaseController
 {
-    //
+    // You can add shared logic here if needed
 }
--- gestion_bibliotheque/routes/web.php
+++ gestion_bibliotheque/routes/web.php
@@ -1,7 +1,48 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BookController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+
+Route::resource('books', BookController::class);
+
+use App\Http\Controllers\AuthController;
+
+Route::get('/register', [AuthController::class, 'showRegister'])->name('register');
+Route::post('/register', [AuthController::class, 'register']);
+
+Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware('auth')->name('dashboard');
+
+use App\Http\Controllers\ProfileController;
+
+Route::middleware(['auth'])->group(function () {
+    // Afficher le profil
+    Route::get('/profile', [ProfileController::class, 'show'])->name('profile.show');
+    
+    // Modifier le profil
+    Route::get('/profile/edit', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::put('/profile', [ProfileController::class, 'update'])->name('profile.update');
 });
